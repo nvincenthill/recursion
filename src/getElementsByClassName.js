67d1aaf9ec54a80873885876c64c8af1554427c7
mypersonalgithubaccount <nvincenthill@gmail.com>
2018-05-28 00:24:06 -0700

    Implement solutions to stringifyJSON and getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,19 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+
+var getElementsByClassName = function(className) {
+  let nodes = [];
+
+  function test(node) {
+    if (node.classList && node.classList.contains(className)) {
+        nodes.push(node);
+    }
+    for (let i = 0; i < node.childNodes.length; i++) {
+      test(node.childNodes[i]);
+    }
+  }
+
+  test(document.body);
+  return nodes;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,52 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  
+  // undefineds
+  if (obj === undefined) return undefined;
+  // nulls
+  else if (obj === null) return "null";
+  // strings
+  else if (typeof obj === 'string') {
+    return '"' + obj.replace(/"/g, '\\"') + '"';
+  }
+  // numbers
+  else if (obj.constructor === Number) return String(obj);
+  // booleans
+  else if (obj.constructor === Boolean) return obj ? "true" : "false";
+  // arrays
+  else if (obj.constructor === Array)
+    return (
+      "[" +
+      obj
+        .reduce((acc, v) => {
+          if (v === undefined) return [...acc, "null"];
+          else return [...acc, stringifyJSON(v)];
+        }, [])
+        .join(",") +
+      "]"
+    );
+  // objects
+  else if (obj.constructor === Object) {
+    if (obj === {}) {
+      return "{}";
+    } else {
+      return (
+        "{" +
+        Object.keys(obj)
+          .reduce((acc, k) => {
+            // check for undefined
+            if (obj[k] === undefined) return acc;
+            // check for function
+            if (typeof obj[k] === 'function') return acc;
+            else
+              return [...acc, stringifyJSON(k) + ":" + stringifyJSON(obj[k])];
+          }, [])
+          .join(",") +
+        "}"
+      );
+    }
+  } 
+
+  else return "{}";
 };
